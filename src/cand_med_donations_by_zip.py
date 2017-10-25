6e41ff1083280975a7b77524903d20c321eb6ef2
andywillcock <andy.willcock@gmail.com>
2017-10-25 15:41:03 -0700

    Open .txt file and readline into numpy recarray

---
+++ src/cand_med_donations_by_zip.py
@@ -0,0 +1,21 @@
+import os
+import numpy as np
+
+filepath = os.getcwd()+"/itcont.txt"
+def medianvals_by_zip(filepath):
+
+
+    with open(filepath,'r') as f:
+        records_dt = np.dtype([('CMTE_ID','|S10'),('ZIP_CODE', '|S10'),('TRANS_DT',np.int64),('TRANS_AMT',np.int64),
+                               ('OTHER_ID', '|S10'),('MEDIAN_AMT_BY_ZIP',np.int64),('DONATION_COUNT',np.int64),('TOTAL_AMT',np.int64)])
+        records = np.recarray((1,),dtype=records_dt)
+        for line in f.readlines():
+            record = line.strip('\n').split('|')
+            relevant_data = [ record[i] for i in [0, 10, 13, 14, 15] ]
+            relevant_data.extend((relevant_data[3],1,relevant_data[3]))
+            relevant_data[1] = relevant_data[1][0:5]
+            relevant_data = np.rec.array(relevant_data,dtype=records_dt)
+
+        print relevant_data
+
+medianvals_by_zip(filepath)

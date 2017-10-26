76f3e4df1ff14f4319b1e1d6df0fb4aecee7b1ee
andywillcock <andy.willcock@gmail.com>
2017-10-26 14:56:22 -0700

    Add docstrings and comments for each function

--- src/cand_med_donations_by_zip.py
+++ src/cand_med_donations_by_zip.py
@@ -5,11 +5,25 @@ import datetime
 filepath = '/Users/ATW/find-political-donors/input/itcont.txt'
 
 def medianvals_by_zip(filepath):
+    """
+    Opens input file of individual political contributions as a stream, reads each line of data, calculates the running median, total
+    number of donations, and total donation amounts for each candidate by zipcode. Writes out a pipe separated txt file
+    with each newline including the candidate ID number, zipcode, and running statistics for the corresponding input row.
 
+    :param filepath: path to data file for input
+    :return: output_records
+    """
     with open(filepath,'r') as f:
+        # Define variable types of each possible column
         records_dt = np.dtype([('CMTE_ID','|S10'),('ZIP_CODE', '|S10'),('TRANS_DT','|S10'),('TRANS_AMT',np.float64),
                                ('OTHER_ID', '|S10'),('MEDIAN_AMT_BY_ZIP',np.int64),('DONATION_COUNT',np.int64),('TOTAL_AMT',np.int64)])
+
+        #Create recarray shell for the records that will be part of the output file
         records = np.recarray((1,),dtype=records_dt)
+
+        # Read data file line by line and add relevant data to a dictionary.
+        # Dictionary Structure = { CMTE_ID : {ZIP_CODE : [TRANS_AMT,TRANS_AMT], ZIP_CODE : [TRANS_AMT,TRANS_AMT]},
+        # CMTE_ID : {TRANS_DT : [TRANS_AMT(s)]}, etc. }
         candidates = {}
         for line in f.readlines():
             record = line.strip('\n').split('|')
@@ -17,14 +31,22 @@ def medianvals_by_zip(filepath):
             relevant_data.extend((relevant_data[3],1,relevant_data[3]))
             relevant_data[1] = relevant_data[1][0:5]
 
+            # Check data for relevancy using the rules defined in the data considerations section
+            # Check that CMTE_ID exists
+            # Check that ZIP_CODE is the correct length and not empty
+            # Check to make sure that the TRANSACTION_AMT is not empty
+            # Check to make sure that OTHER_ID is empty
+            # If any of these are true the row is skipped
             if relevant_data[0] == '' \
-                    or len(relevant_data[1]) != 5 \
+                    or len(relevant_data[1].strip(" ")) != 5 or relevant_data[1].strip(" ") == ''\
                     or relevant_data[3] == '' \
                     or relevant_data[4] != '':
                 continue
 
+            # Create numpy recarray of relevant data for value easier accession
             relevant_data = np.rec.array(relevant_data,dtype=records_dt)
 
+            # Fill in dictionary with each unique candidate/zip code and that zip code's transaction amounts
             if relevant_data.CMTE_ID.item() in candidates.keys():
 
                 if relevant_data.ZIP_CODE.item() in candidates[relevant_data.CMTE_ID.item()].keys():
@@ -34,24 +56,41 @@ def medianvals_by_zip(filepath):
             else:
                 candidates[relevant_data.CMTE_ID.item()] = {relevant_data.ZIP_CODE.item():[relevant_data.TRANS_AMT.item()]}
 
+            # Calculate fields of interest and store them in the proper columns of the recarray
             relevant_data.MEDIAN_AMT_BY_ZIP = round(np.median(candidates[relevant_data.CMTE_ID.item()][relevant_data.ZIP_CODE.item()]).item(), 0)
             relevant_data.TOTAL_AMT = np.sum(candidates[relevant_data.CMTE_ID.item()][relevant_data.ZIP_CODE.item()])
             relevant_data.DONATION_COUNT = len(candidates[relevant_data.CMTE_ID.item()][relevant_data.ZIP_CODE.item()])
 
+            # Add this data to the records array
             records = np.vstack((records, relevant_data))
 
+            # Create numpy array of all relevant records and strip the first row which was present as a placeholder
             output_records = np.hstack((records['CMTE_ID'], records['ZIP_CODE'], records['MEDIAN_AMT_BY_ZIP'],
                                         records['DONATION_COUNT'], records['TOTAL_AMT']))
             output_records = np.delete(output_records, (0), axis=0)
 
+            # Output output_records array to the correct folder as medianvals_by_date.txt
             np.savetxt(os.path.dirname(os.getcwd())+'/output/medianvals_by_zip.txt',
                        output_records, delimiter='|', fmt="%s")
 
+
+    # Close input data file
     f.close()
+    return output_records
 
 medianvals_by_zip(filepath)
 
 def medianvals_by_date(filepath):
+    """
+    Opens input file of individual political contributions as a stream, reads each line of data, calculates the median,
+    total number of donations, and total donation amounts for each candidate by date. Writes out a pipe separated txt
+    file with each newline including the candidate ID number, date, and calculated statistics for each unique date
+    for a candidate
+
+    :param filepath: path to data file for input
+    :return: output_records: numpy array of candidate ID number, date, and calculated statistics. Each row is a unique
+    date for a given candidate
+    """
 
     with open(filepath, 'r') as f:
 
@@ -61,7 +100,7 @@ def medianvals_by_date(filepath):
                                ('TOTAL_AMT', np.int64)])
 
         # Read data file line by line and add relevant data to a dictionary.
-        # Dictionary Structure = { CMTE_ID : {TRANS_DT : [TRANS_AMT(s)]}, CMTE_ID : {TRANS_DT : [TRANS_AMT(s)]}, etc. }
+        # Dictionary Structure = { CMTE_ID : {TRANS_DT : [TRANS_AMT,TRANS_AMT]}, CMTE_ID : {TRANS_DT : [TRANS_AMT(s)]}, etc. }
         candidates = {}
 
         # Read each line, split into a list using separator |, pull necessary data out, add placeholders for data
@@ -80,7 +119,7 @@ def medianvals_by_date(filepath):
             # Check to make sure that OTHER_ID is empty
             # If any of these are true the row is skipped
             if relevant_data[0] == '' \
-                    or relevant_data[2] == '' or len(relevant_data[2]) != 8 or int(relevant_data[2][-4:]) > now.year  \
+                    or relevant_data[2] == '' or len(relevant_data[2].strip(" ")) != 8 or int(relevant_data[2][-4:]) > now.year  \
                     or relevant_data[3] == '' \
                     or relevant_data[4] != '':
 
@@ -89,7 +128,7 @@ def medianvals_by_date(filepath):
             # Create numpy recarray of relevant data for value easier accession
             relevant_data = np.rec.array(relevant_data, dtype=records_dt)
 
-            # Fill in dictionary with each unique candidate/zip code and that zip codes transactions
+            # Fill in dictionary with each unique candidate/date and that date's transaction amounts
             if relevant_data.CMTE_ID.item() in candidates.keys():
 
                 if relevant_data.TRANS_DT.item() in candidates[relevant_data.CMTE_ID.item()].keys():
@@ -102,8 +141,8 @@ def medianvals_by_date(filepath):
                 candidates[relevant_data.CMTE_ID.item()] = {
                     relevant_data.TRANS_DT.item(): [relevant_data.TRANS_AMT.item()]}
 
-        #Create Outputfile using loop to go through each date for each candidate create a row for that candidate/date
-        # and then filling the row with the appropriate stats
+        #Create output_array using loop. Loops thorugh each date for each candidate, creates a row for that candidate/date
+        # and then filling the row with the appropriate statistics
         output_records = []
         for cand in candidates.keys():
             for key in candidates[cand].keys():
@@ -114,11 +153,14 @@ def medianvals_by_date(filepath):
                 row = [cand,date,str(median),str(count),str(total)]
                 output_records.append(row)
 
+        # Create numpy array of all records
         output_records = np.array(output_records,dtype = '|S10')
 
+        # Output output_records array to the correct folder as medianvals_by_date.txt
         np.savetxt(os.path.dirname(os.getcwd()) + '/output/medianvals_by_date.txt', output_records,
                    delimiter='|', fmt="%s")
 
+    # Close input data file
     f.close()
 
 medianvals_by_date(filepath)
